1903c89d0a20f2d912d39e38824e15e2aaae09ce
Prem <170030111@kluniversity.in>
2021-06-22 18:10:16 +0530

    Hibernate Using Annotations and maven

---
+++ HibernateAnnotation/src/main/java/com/vm/HibernateAnnotation/App.java
@@ -0,0 +1,13 @@
+package com.vm.HibernateAnnotation;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
